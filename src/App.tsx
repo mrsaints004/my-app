9950b92b0ece30434d84ddf70712b089ec88e282
mrsaints004 <mrsaints004@gmail.com>
2024-05-09 05:44:54 +0100

    Good job

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useWeb3Modal } from '@web3modal/wagmi/react'
+
+export default function App() {
+  // 4. Use modal hook
+  const { open } = useWeb3Modal()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button onClick={() => open()}>Connect Wallet</button>
+      <button onClick={() => open({ view: 'Networks' })}> Select Networks</button>
+    </>
+  )
 }
+    
+    
 
-export default App;
+
+
